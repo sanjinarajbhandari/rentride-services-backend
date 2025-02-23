c7e5dec5e929a8d8aea8729c82dfa4fbc6b3f3a8
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-02-23 14:13:08 +0545

    Updated index.js

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const cookieParser = require("cookie-parser");
 const url = process.env.MONGO_DB_CONNECTION;
 
 //local imports
-const bookingRoutes = require("./routes/bookingRoutes");
-const vehicleRoutes = require("./routes/vehicleRoutes");
-const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
-const availabilityUpdate = require("./utlis/schedule");
-const khaltiRoutes = require("./routes/khaltiRoutes");
-const checkoutRoutes = require("./routes/checkoutRoute");
 const whitelist = process.env.WHITELISTED_DOMAINS;
 
 const app = express();
@@ -35,11 +29,6 @@ app.use(express.json());
 
 // Routes Middleware
 app.use(userRoutes);
-app.use(productRoutes);
-app.use(vehicleRoutes);
-app.use(bookingRoutes);
-app.use(khaltiRoutes);
-app.use(checkoutRoutes);
 
 //Database Connection
 mongoose
@@ -49,6 +38,5 @@ mongoose
     app.listen(process.env.PORT || 8081, () => {
       console.log(`Listening to PORT : ${process.env.PORT}`);
     });
-    availabilityUpdate();
   })
   .catch((error) => console.log(error));
