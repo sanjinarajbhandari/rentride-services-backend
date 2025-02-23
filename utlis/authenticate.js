ad797f1e1bca6ff6eaa7addd28ae936eed1e1f97
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-02-23 17:44:49 +0545

    Added utils related to users

---
+++ utlis/authenticate.js
@@ -0,0 +1,33 @@
+require("dotenv").config;
+const jwt = require("jsonwebtoken");
+
+exports.getToken = (user) => {
+  return jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+exports.getRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+};
+
+//create a token for resetting password
+exports.getPasswordResetToken = (user) => {
+  return jwt.sign(user, process.env.PASSWORD_RESET_TOKEN_SECRET, {
+    expiresIn: process.env.PASSWORD_RESET_TOKEN_EXPIRY,
+  });
+};
+
+
+exports.validateToken = (req, res, next) => {
+  const header = req.headers["authorization"];
+  const token = header && header.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    console.log("valid token");
+    next();
+  });
+};
