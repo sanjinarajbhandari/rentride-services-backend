64282742f9b9bb13d79514a6e8d0a18f7022c9e9
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-03-30 01:15:20 +0545

    Updated user route logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,6 @@ router.post("/login", userController.login);
 router.get("/fetchUser", userController.fetchUser);
 
 router.get("/checkAdmin", validateAdmin(), (req, res) => {
-  console.log(req.userRole, "up[puo");
   const userRole = req.userRole;
   if (userRole === "user") {
     res.status(200).json({ userRole, message: "User is admin" });
@@ -18,7 +17,10 @@ router.get("/checkAdmin", validateAdmin(), (req, res) => {
     res.status(403).json({ userRole, message: "User is not admin" });
   }
 });
-// Route for user change Password 
+router.post("/passwordReset", userController.passwordReset);
+// Route for user Password Reset
+router.post("/resetPassword", userController.resetpassword);
+// Route for user change Password
 router.post("/changePassword", userController.changePassword);
 
 // Route for generating new token once the token is expires
@@ -26,5 +28,17 @@ router.post("/token", userController.token);
 
 // Route for user logout
 router.get("/logout", userController.logout);
+
+router.get("/getDashboard", userController.getDashboardSummary);
+
+router.get("/getVendors", userController.getVendors);
+
+router.post("/addVendor", validateAdmin(), userController.addVendor);
+
+router.delete(
+  "/deleteVendor/:vendorId",
+  validateAdmin(),
+  userController.deleteVendor
+);
 
 module.exports = router;
